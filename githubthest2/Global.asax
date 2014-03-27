967a58edbedeec3217ae9642b8b737f083f5ce20
alissay <alissa.young@gmail.com>
2014-03-27 11:34:54 -0400

    Initial Commit

---
+++ githubthest2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="githubthest2.MvcApplication" Language="C#" %>
---
+++ githubthest2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(githubthest2.Startup))]
+namespace githubthest2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
